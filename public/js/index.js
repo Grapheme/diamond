4dde9cb42d01d29e305e34791ef2edc10d00b12c
molefrog <molefrog@gmail.com>
2013-10-15 11:32:49 +0400

    Fixed relative paths

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,12 @@
 $(window).on("load", function() {
+	var part =  location.pathname.substring(0, location.pathname.lastIndexOf("/")+1);
+	if(part[0] === '/') {
+		part = part.substring(1);
+	}
 
-	var socketAddress = location.origin + location.pathname.substring(0, location.pathname.lastIndexOf("/")+1);
-
-	 var socket = io.connect(socketAddress);
+	var socket = io.connect(location.origin, {
+		resource : part + "socket.io"
+	});
 
 	var diamondImage = $("#diamond")[0];
 
--- public/js/mover.js
+++ public/js/mover.js
@@ -1,7 +1,12 @@
 $(window).on("load", function() {
-	var socketAddress = location.origin + location.pathname.substring(0, location.pathname.lastIndexOf("/")+1);
+	var part =  location.pathname.substring(0, location.pathname.lastIndexOf("/")+1);
+	if(part[0] === '/') {
+		part = part.substring(1);
+	}
 
-	var socket = io.connect(socketAddress);
+	var socket = io.connect(location.origin, {
+		resource : part + "socket.io"
+	});
 
 	var demo = Sketch.create({
 		container: document.getElementById( 'container' )
